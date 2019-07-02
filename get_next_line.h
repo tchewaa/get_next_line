f68f517ada0eddb3543aa02309f98a5638737e75
Thabang CHEWA <tchewa@c4r12s1.wethinkcode.co.za>
2019-07-02 10:02:45 +0200

    added buff size 32

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,13 @@
 /*   By: tchewa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/30 12:33:33 by tchewa            #+#    #+#             */
-/*   Updated: 2019/06/30 15:16:59 by tchewa           ###   ########.fr       */
+/*   Updated: 2019/07/02 10:02:04 by tchewa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
 
 # include "libft/libft.h"
 
